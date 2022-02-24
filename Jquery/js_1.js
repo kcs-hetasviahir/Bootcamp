29c0e6facbdd725057ad4b4d6cc36b47926f469f
Hetasvi Ahir <hetasvi.ahir@kcsitglobal.com>
2022-02-24 18:03:31 +0530

    Initial Commit

---
+++ Jquery/js_1.js
@@ -0,0 +1,13 @@
+            function Alert1()
+            {
+                alert("Internal Alert");
+            }
+            function Confirm1()
+            {
+                alert("Internal Confirm");
+            }
+            function Prompt1()
+            {
+                var fName=prompt("Enter your name:");
+                alert(fName);
+            }
